b8a54442ba46829f5e2f95354f8307c83cd4ae76
Tarciso Torres <tarcisoftorres@gmail.com>
2019-10-03 16:27:06 -0300

    Inserção de usuário com POST

--- src/main/java/com/redfort/workshopmongo/services/UserService.java
+++ src/main/java/com/redfort/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.redfort.workshopmongo.domain.User;
+import com.redfort.workshopmongo.dto.UserDTO;
 import com.redfort.workshopmongo.exception.ObjectNotFoundException;
 import com.redfort.workshopmongo.repository.UserRepository;
 
@@ -19,10 +20,18 @@ public class UserService {
 	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 
 }
