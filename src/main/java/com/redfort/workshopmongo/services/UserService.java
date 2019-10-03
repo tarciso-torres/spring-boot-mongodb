5e40a8b940baff3d8cfb2d74079ff8fcb122868a
Tarciso Torres <tarcisoftorres@gmail.com>
2019-10-03 16:56:31 -0300

    Atualização de usuário com PUT

--- src/main/java/com/redfort/workshopmongo/services/UserService.java
+++ src/main/java/com/redfort/workshopmongo/services/UserService.java
@@ -25,18 +25,29 @@ public class UserService {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
-	
+
 	public User insert(User obj) {
 		return userRepository.insert(obj);
 	}
 
-	public User fromDTO(UserDTO objDTO) {
-		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
-	}
-	
 	public void delete(String id) {
 		findById(id);
 		userRepository.deleteById(id);
+	}
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return userRepository.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 
 }
