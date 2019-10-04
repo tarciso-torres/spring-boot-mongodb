b00b08dff49a816031f111ccd6d11bad49747f7d
Tarciso Torres <tarcisoftorres@gmail.com>
2019-10-04 16:05:17 -0300

    Obtendo um post por id

---
+++ src/main/java/com/redfort/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.redfort.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.redfort.workshopmongo.domain.Post;
+import com.redfort.workshopmongo.exception.ObjectNotFoundException;
+import com.redfort.workshopmongo.repository.PostRepository;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
+}
