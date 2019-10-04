e67b2c96b89e9a6ae5b84ae3ccd5e00dd9d70274
Tarciso Torres <tarcisoftorres@gmail.com>
2019-10-04 17:02:54 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/redfort/workshopmongo/domain/Post.java
+++ src/main/java/com/redfort/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.redfort.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.redfort.workshopmongo.dto.AuthorDTO;
+import com.redfort.workshopmongo.dto.CommentDTO;
 
 @Document(collection = "post")
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 		super();
@@ -71,6 +76,10 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
 	}
 
 	@Override
